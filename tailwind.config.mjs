3aa3cea6d2944978e74650aa90dc612d799d9f1d
Miv Anna Donkaewpoo Berg <mibe0002@stud.kea.dk>
2025-05-21 15:45:33 +0200

    text test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,33 +8,30 @@ export default {
     // overwrite indstillinger her
     extend: {
       // extend indstillinger her
-
       /* Tilføj din egen farvepalette nedenfor */
-      colors: {
-        // transparent: "transparent",
-        // current: "currentColor",
-        // brun: {
-        //   0: "#FFFFFF",
-        //   25: "#FAF9F8",
-        //   50: "#F6F1EF",
-        //   100: "#ECE3DF",
-        //   200: "#DBC9C2",
-        //   300: "#C8ADA2",
-        //   400: "#B79485",
-        //   500: "#A57865",
-        //   600: "#87604F",
-        //   700: "#64473A",
-        //   800: "#443028",
-        //   900: "#201713",
-        // },
-      },
-
-      /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
-      // fontFamily: {
-      //   outfit: ["outfit", "sans-serif"],
-      //   agrandir: ["agrandir", "sans-serif"],
+      // colors: {
+      //   transparent: "transparent",
+      //   current: "currentColor",
+      //   brun: {
+      //     0: "#FFFFFF",
+      //     25: "#FAF9F8",
+      //     50: "#F6F1EF",
+      //     100: "#ECE3DF",
+      //     200: "#DBC9C2",
+      //     300: "#C8ADA2",
+      //     400: "#B79485",
+      //     500: "#A57865",
+      //     600: "#87604F",
+      //     700: "#64473A",
+      //     800: "#443028",
+      //     900: "#201713",
+      //   },
       // },
-
+      /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
+      fontFamily: {
+        outfit: ["outfit", "sans-serif"],
+        agrandir: ["agrandir", "sans-serif"],
+      },
       /* Borders - borderstørrelser herunder */
       // borderWidth: {
       //   default: "0",
